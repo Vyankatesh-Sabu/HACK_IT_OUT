e8657d59129c00fd2250be4da734d3e356f10466
Vyankatesh Sabu <vrsabu545@gmail.com>
2025-03-01 05:06:26 +0530

    created animated loading screen

--- app/src/main/java/com/example/drservice/screen/Loading.kt
+++ app/src/main/java/com/example/drservice/screen/Loading.kt
@@ -1,2 +1,43 @@
 package com.example.drservice.screen
 
+
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.LottieConstants
+import com.example.drservice.R
+
+@Composable
+fun Loading() {
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.bouncing_circles))
+
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ){
+        LottieAnimation(
+            composition = composition,
+            iterations = LottieConstants.IterateForever, // Loop animation
+            modifier = Modifier.size(100.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoadingPreview() {
+    Loading()
+}
