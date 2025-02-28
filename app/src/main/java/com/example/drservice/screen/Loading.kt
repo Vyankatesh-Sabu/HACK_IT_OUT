b01a7333f6b3232199794ecaa3526cee1306ff96
Vyankatesh Sabu <vrsabu545@gmail.com>
2025-02-28 18:07:35 +0530

    Loading Screen designing

---
+++ app/src/main/java/com/example/drservice/screen/Loading.kt
@@ -0,0 +1,10 @@
+package com.example.drservice.screen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+
+fun Loading(){
+    Column {
+        Image()
+    }
+}
